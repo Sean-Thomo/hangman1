64254fa599f807d3f6b55219bf8552a2a31061f5
Sean Thomo <seansthomo@gmail.com>
2023-02-22 17:11:18 +0200

    generate random index

--- Hangman.java
+++ Hangman.java
@@ -1,12 +1,14 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Hangman {
     public static void main(String[] args) {
         try {
-            String[] wordsArray;
+
+            ArrayList<String> wordsArray = new ArrayList<String>();
             File myObj = new File("shortwords.txt");
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
@@ -14,9 +16,18 @@ public class Hangman {
                 wordsArray.add(word);
             }
             myReader.close();
+
+            int index = getRandomInt(0, wordsArray.size());
+
         } catch (FileNotFoundException e) {
             System.out.println("An error occured.");
             e.printStackTrace();
         }
     }
+
+    static int getRandomInt(int min, int max){
+        Random num = new Random();
+        return num.nextInt((max - min) + 1) + min;
+    }
+    
 }
